6db57e20c8822a54b5c8b5b254bb2973a5d094be
BostoenToby <toby.bostoen@student.howest.be>
2022-09-27 12:12:30 +0200

    feat: NestJS setup

---
+++ api/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  const port: number = +process.env.PORT|| 3003
+
+  await app.listen(port)
+  console.info(`🖐️\nWelcome to the server.\nVisit http://localhost:${port}/`)
+}
+bootstrap()
